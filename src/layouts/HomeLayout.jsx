b404427ff06b80870f270342084ec8aa6337f692
tammykhan <tammykhan543@gmail.com>
2024-11-17 13:38:40 +0600

    Fetch and showed category data

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
 import Navbar from '../components/Navbar';
+import LeftNavbar from '../components/layout/LeftNavbar';
 
 const HomeLayout = () => {
   return (
@@ -11,13 +12,16 @@ const HomeLayout = () => {
           <section className='w-11/12 mx-auto'>
              <LatestNews></LatestNews>
           </section>
-        
        </header>
+
        <nav className='w-11/12 mx-auto py-2'>
        <Navbar></Navbar>  
        </nav>
+
        <main className='w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-2'>
-           <aside className='left col-span-3'>Left Navbar</aside>
+           <aside className='left col-span-3'>
+            <LeftNavbar></LeftNavbar>
+           </aside>
            <section className='col-span-6'>Main Content</section>
            <aside className='col-span-3'>Right Navbar</aside>
        </main>
