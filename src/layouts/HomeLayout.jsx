f1e519d6c13bb261252de0ad48560cb31ea57533
tammykhan <tammykhan543@gmail.com>
2024-11-17 11:43:17 +0600

    navbar and main layout done

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
+import Navbar from '../components/Navbar';
 
 const HomeLayout = () => {
   return (
-    <div>
+    <div className='font-poppins'>
        <header>
           <Header></Header>
           <section className='w-11/12 mx-auto'>
              <LatestNews></LatestNews>
           </section>
+        
        </header>
-       <nav></nav>
-       <main></main>
+       <nav className='w-11/12 mx-auto py-2'>
+       <Navbar></Navbar>  
+       </nav>
+       <main className='w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-2'>
+           <aside className='left col-span-3'>Left Navbar</aside>
+           <section className='col-span-6'>Main Content</section>
+           <aside className='col-span-3'>Right Navbar</aside>
+       </main>
     </div>
   );
 };
